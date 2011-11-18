ca78902f85feb0edb7a2d9dd55a783345e77f8d1
psd <psd@whatfettle.com>
2011-11-18 10:58:19 +0000

    initial hack based on ColorHug project

---
+++ parts2bom.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+awk -F'	' '
+	BEGIN {
+		print "{\n	 \"items\: ["
+		sep = ""
+	}
+	/^[^\.]/ {
+		printf sep
+		printf "		{\n"
+		printf "			\"designator\": \"%s\",\n", $1
+		printf "			\"value\": \"%s\",\n", $3
+		printf "			\"description\": \"%s (%s)\",\n", $2, $4
+		printf "		}"
+		sep = ",\n"
+	}
+	END {
+		print "\n	]\n}"
+	}
+'
